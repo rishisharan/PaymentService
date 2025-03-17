8e891bf9876ecd10f96e458d1b034a415977d8e6
rishisharan pentala <rishisharanpentala@gmail.com>
2025-03-17 13:09:18 -0400

    first commit for payment service

---
+++ src/main/java/com/paymentservice/model/Payment.java
@@ -0,0 +1,12 @@
+package com.paymentservice.model;
+
+public class Payment {
+	private String orderId;
+	private String paymentId;
+	private String userId;
+	private double amount;
+	private String paymentMethod;
+	private String currency;
+	private String status;
+	
+}
